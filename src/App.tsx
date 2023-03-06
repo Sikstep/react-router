e919d7a24c9325c1dab01224d4db4a812c96fac5
Sikstep <Sikstep1988@gmail.com>
2023-03-06 20:24:05 +0300

    Links

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,53 @@
 import React from 'react';
 import './App.css';
-import {NavLink, Outlet, Route, Routes, useParams} from 'react-router-dom';
+import {NavLink, Route, Routes} from 'react-router-dom';
+import s from './App.module.css'
 
-const Profile = () => {
-
-    const params = useParams<'x' | 'y'>()
-
-    const some = params
-
-    console.log(some)
-
-    return <div>profile</div>
-}
+// const Profile = () => {
+//
+//     const params = useParams<'x' | 'y'>()
+//
+//     const some = params
+//
+//     console.log(some)
+//
+//     return <div>profile</div>
+// }
 
 function App() {
     return (
         <div className="App">
             <NavLink to={'/'}>main</NavLink>---
             <NavLink to={'/login'}>login</NavLink>---
-            <NavLink to={'/profile'}>profile</NavLink>---
-            <NavLink to={'/profile/1'}>profile/1</NavLink>
+
+            <NavLink
+                to={'/profile'}
+                style={(params) => {
+                    return {color: params.isActive ? 'lightblue' : ''}
+                }}>
+                profile
+            </NavLink>---
+            <NavLink
+                to={'/profile/settings'}
+                className={(params) => params.isActive ? s.act : s.def}
+                >
+                settings
+            </NavLink>---
+
+            <a
+                href={'https://github.com/IgnatZakalinsky/simple-react-homeworks'}
+                target={'_blank'}
+                rel={'noreferrer nofollow noopener'}
+            >
+                xxx
+            </a>
 
             <Routes>
                 <Route path={'/*'} element={<div>404</div>}/>
                 <Route path={'/'} element={<div>main</div>}/>
                 <Route path={'/login'} element={<div>login</div>}/>
-                <Route path={'/profile/:x/:y'} element={<Profile/>}/>
+                <Route path={'/profile'} element={<div>profile</div>}/>
+                <Route path={'/profile/settings'} element={<div>settings</div>}/>
             </Routes>
 
 
