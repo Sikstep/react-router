f6fb9616ce5001f080ab55804e40777e4df844cb
Sikstep <Sikstep1988@gmail.com>
2023-03-06 18:02:08 +0300

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {NavLink, Route, Routes} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+            <NavLink to={'/'}>main</NavLink>---
+            <NavLink to={'/login'}>login</NavLink>---
+            <NavLink to={'/profile'}>profile</NavLink>---
+            <NavLink to={'/profile/settings'}>settings</NavLink>
 
-    </div>
-  );
+            <Routes>
+                <Route path={'/*'} element={<div>404</div>}/>
+                <Route path={'/'} element={<div>main</div>}/>
+                <Route path={'/login'} element={<div>login</div>}/>
+                <Route path={'/profile'} element={<div>profile</div>}/>
+                <Route path={'/profile/settings'} element={<div>settings</div>}/>
+            </Routes>
+
+        </div>
+    );
 }
 
 export default App;
