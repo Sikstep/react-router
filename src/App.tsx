14be398f86b82abd70c6a291a6d4d4dbb76adc33
Sikstep <Sikstep1988@gmail.com>
2024-04-15 13:58:58 +0300

    BrowserRouter new

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React, {useState} from 'react';
-import './App.css';
-import {Site} from "./components/Site";
+import styles from "./components/Site.module.css";
 
 
 function App() {
     return (
         <div>
-            <Site/>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    Здесь будет навигация
+                </div>
+                <div className={styles.content}>
+                    Здесь будут кроссовки
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
         </div>
     );
 }
